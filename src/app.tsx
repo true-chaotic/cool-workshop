3d91ad54d1f64578d3c200ee62340e6f87e9d94c
Samokhin Alex <truechaoticphone@gmail.com>
2021-08-14 19:40:40 +0300

    fix test errors

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import * as ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom';
 
 function render() {
-    ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById('container'));
+  ReactDOM.render(<h2>Hello from React!</h2>, document.getElementById('container'));
 }
 
 render();
